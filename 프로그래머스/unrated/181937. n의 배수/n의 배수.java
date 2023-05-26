7e2a6cac75ae1fb1edcdee472d61cb4e90044f87
Ainchel <134568940+Ainchel@users.noreply.github.com>
2023-05-26 18:40:54 +0900

    [unrated] Title: n의 배수, Time: 0.02 ms, Memory: 77.9 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181937. n의 배수/n의 배수.java
@@ -0,0 +1,16 @@
+class Solution 
+{
+    public int solution(int num, int n) 
+    {
+        int Result = -1;
+    	
+        if(num % n == 0)
+        	Result = 1;
+        
+        else
+        	Result = 0;
+        
+        int answer = Result;
+        return answer;
+    }
+}
