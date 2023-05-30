51b3758077d2eb5290d8353f6dc2535775574bc4
Ainchel <134568940+Ainchel@users.noreply.github.com>
2023-05-30 10:05:45 +0900

    [level 0] Title: 숫자 비교하기, Time: 0.02 ms, Memory: 75.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120807. 숫자 비교하기/숫자 비교하기.java
@@ -0,0 +1,16 @@
+class Solution 
+{
+    public int solution(int num1, int num2) 
+    {
+    	int Result = 0;
+    	
+        if(num1 == num2)
+        	Result = 1;
+        
+        else if (num1 != num2)
+        	Result = -1;
+        	
+        int answer = Result;
+        return answer;
+    }
+}
