a4370e6cdc03ad34283b4be8affe7645beadf19a
Ainchel <134568940+Ainchel@users.noreply.github.com>
2023-05-31 11:30:53 +0900

    [level 0] Title: 편지, Time: 0.04 ms, Memory: 74.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120898. 편지/편지.java
@@ -0,0 +1,11 @@
+class Solution 
+{
+    	public int solution(String message) 
+    {
+        int CharCount = message.toCharArray().length;
+        
+        int answer = CharCount * 2;
+        return answer;
+    }
+	
+}
