3f74532b38a7cf44101e56b00a7835b614b958a1
Ainchel <134568940+Ainchel@users.noreply.github.com>
2023-05-31 13:22:27 +0900

    [level 0] Title: 아이스 아메리카노, Time: 0.02 ms, Memory: 69.9 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120819. 아이스 아메리카노/아이스 아메리카노.java
@@ -0,0 +1,14 @@
+class Solution 
+{
+     public int[] solution(int money) 
+	 {
+		 int P = 5500;
+		 int IndexCount = 2;
+		 int M = money / 5500 ;
+		 int N = money % 5500 ;
+		 int[] Result = {M, N};
+		 
+	        int[] answer = Result;
+	        return answer;
+	 }
+}
