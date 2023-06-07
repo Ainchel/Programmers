b13be6a3096de378145ebb517624a03c9979e738
Ainchel <134568940+Ainchel@users.noreply.github.com>
2023-06-07 17:44:06 +0900

    [unrated] Title: rny_string, Time: 0.07 ms, Memory: 72.3 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181863. rny＿string/rny＿string.java
@@ -0,0 +1,10 @@
+class Solution {
+    public String solution(String rny_string) 
+	 {
+		 String result = rny_string;
+		 result = result.replaceAll("m", "rn");
+		 
+	     String answer = result;
+	     return answer;
+	 }
+}
