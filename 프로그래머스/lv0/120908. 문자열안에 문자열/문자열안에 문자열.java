0227734d0ff630c98a4c56ca704006146f98b913
Ainchel <134568940+Ainchel@users.noreply.github.com>
2023-06-13 14:30:49 +0900

    [level 0] Title: 문자열안에 문자열, Time: 0.03 ms, Memory: 75.9 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120908. 문자열안에 문자열/문자열안에 문자열.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int solution(String str1, String str2) 
+	{
+		int result = -1;
+		
+		if(str1.contains(str2)) result = 1;
+		
+		else result = 2;
+			
+        int answer = result;
+        return answer;
+    }
+}
