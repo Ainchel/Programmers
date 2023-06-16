4f3b047a89537ea08a541d262b8d871a7a66f0d2
Ainchel <134568940+Ainchel@users.noreply.github.com>
2023-06-16 11:59:26 +0900

    [level 0] Title: 주사위의 개수, Time: 0.01 ms, Memory: 74.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120845. 주사위의 개수/주사위의 개수.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int solution(int[] box, int n) 
+	{
+		int result = (box[0] / n) * (box[1] / n) * (box[2] / n);
+		
+        int answer = result;
+        return answer;
+    }
+}
