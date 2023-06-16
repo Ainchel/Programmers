7956614cc24a03ed5bb06db072252351866fdc68
Ainchel <134568940+Ainchel@users.noreply.github.com>
2023-06-16 16:13:59 +0900

    [level 0] Title: n의 배수 고르기, Time: 0.03 ms, Memory: 72.1 MB -BaekjoonHub

--- 프로그래머스/lv0/120905. n의 배수 고르기/n의 배수 고르기.java
+++ 프로그래머스/lv0/120905. n의 배수 고르기/n의 배수 고르기.java
@@ -1,33 +1,6 @@
 class Solution {
-    public int[] solution(int n, int[] numlist) 
-	{
-		int indexCount = 0;
-		int[] numcopy = numlist;
-		
-		for(int i = 0; i < numcopy.length; i++)
-		{
-			if(numcopy[i] % n == 0)indexCount++;
-		}
-		
-		int[] result = new int[indexCount];
-		
-		for(int i = 0; i < result.length; i++)
-		{
-			for(int j = 0; j < numcopy.length; j++)
-			{
-				if(numcopy[j] % n == 0)
-				{
-					if(numcopy[j] == -9999)
-						continue;
-					
-					result[i] = numcopy[j];
-					numcopy[j] = -9999;
-					break;
-				}	
-			}
-		}
-		
-		int[] answer = result;
+    public int[] solution(int n, int[] numlist) {
+        int[] answer = {};
         return answer;
     }
 }
