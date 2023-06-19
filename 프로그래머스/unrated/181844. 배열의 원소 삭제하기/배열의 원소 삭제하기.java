792065c6e23d0066c15e847618ee3bddd791a6ab
Ainchel <134568940+Ainchel@users.noreply.github.com>
2023-06-19 19:00:22 +0900

    [unrated] Title: 배열의 원소 삭제하기, Time: 0.01 ms, Memory: 77.6 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181844. 배열의 원소 삭제하기/배열의 원소 삭제하기.java
@@ -0,0 +1,50 @@
+class Solution {
+    public static void main(String[] args)
+	{
+		System.out.println("Hello World");
+	}
+	
+	public int[] solution(int[] arr, int[] delete_list) 
+	{
+		int indexCount = arr.length;
+		int idx = 0;
+		boolean isSame = false;
+		
+		for(int i = 0; i < arr.length; i++)
+		{
+			for(int j = 0; j < delete_list.length; j++)
+			{
+				if(arr[i] == delete_list[j]) 
+				{
+					indexCount--;
+					break;
+				}
+			}
+		}
+		
+		int[] result = new int[indexCount];
+		
+		for(int i = 0; i < arr.length; i++)
+		{
+			isSame = false;
+			for(int j = 0; j < delete_list.length; j++)
+			{
+				if(arr[i] == delete_list[j])
+					{
+						isSame = true;
+						break;
+					}
+			}
+			
+			if(isSame == false)
+			{
+				result[idx] = arr[i];
+				idx++;
+			}
+				
+		}
+		
+        int[] answer = result;
+        return answer;
+    }
+}
