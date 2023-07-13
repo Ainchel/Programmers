3ce9a946f3f99426339c617fd26cf9aea441e4d6
Ainchel <134568940+Ainchel@users.noreply.github.com>
2023-07-13 23:33:16 +0900

    [level 1] Title: 평균 구하기, Time: 0.05 ms, Memory: 74.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12944. 평균 구하기/평균 구하기.java
@@ -0,0 +1,16 @@
+class Solution {
+    public double solution(int[] arr) 
+	 {
+		 double result = 0;
+		 
+		 for(int i = 0; i < arr.length; i++)
+		 {
+			 result += arr[i];
+		 }
+		 
+		 result /= arr.length;
+		 
+	     double answer = result;
+	     return answer;
+	 }
+}
