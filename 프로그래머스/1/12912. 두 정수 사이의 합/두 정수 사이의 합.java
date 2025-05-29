6f7f5a614dd5caaf62cc72d602b802bd6dea440e
Ainchel <134568940+Ainchel@users.noreply.github.com>
2025-05-29 14:55:28 +0900

    [level 1] Title: 두 정수 사이의 합, Time: 9.95 ms, Memory: 90.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/12912. 두 정수 사이의 합/두 정수 사이의 합.java
@@ -0,0 +1,21 @@
+class Solution {
+    public long solution(int a, int b) {
+        long answer = 0;
+        long addValue = 0;
+        
+        if(a == b){
+            answer = a;
+        } else if(a > b){
+            for(int i = b+1; i < a; i++){
+                answer += i;
+            }
+            answer += a + b;
+        } else if(b > a){
+            for(int i = a+1; i < b; i++){
+                answer += i;
+            }
+            answer += a + b;
+        }
+        return answer;
+    }
+}
