562457cfc349f14e68b1fa5bba4561ad9317fc44
Ainchel <134568940+Ainchel@users.noreply.github.com>
2025-07-08 21:32:02 +0900

    [level 0] Title: 제곱수 판별하기, Time: 0.04 ms, Memory: 86.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/120909. 제곱수 판별하기/제곱수 판별하기.java
@@ -0,0 +1,10 @@
+import java.lang.Math;
+
+class Solution {
+    public int solution(int n) {
+       
+        if(Math.sqrt(n) == (int)Math.sqrt(n)) return 1;
+        else return 2;
+        
+    }
+}
